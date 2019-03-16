56491d607d96afd3dd5a03dd53331d30d8d1e948
Nate Tan <nktan93@gmail.com>
2019-03-16 02:33:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,12 @@
 require 'pry'
 
-def line(katz_deli)
+# the next number on wheel is 2355
+# no parameters 
+# the line is currently [2353, 2354]
+
+$katz_deli = [2353, 2354]
+
+def line
   new_list = []
   katz_deli.each_with_index do |element, index|
     new_list << "#{index + 1}. #{element}"
@@ -8,16 +14,16 @@ def line(katz_deli)
   if katz_deli.length === 0
     puts "The line is currently empty."
   else
-    puts "The line is currently: #{new_list.join(" ")}"
+    puts "The line is currently: #{katz_deli}"
   end
 end
 
-def take_a_number(katz_deli, end_of_line)
+def take_a_number
   katz_deli << end_of_line
   puts "Welcome, #{end_of_line}. You are number #{katz_deli.length} in line."
 end
 
-def now_serving(katz_deli)
+def now_serving
   if katz_deli.length === 0
     puts "There is nobody waiting to be served!"
   else
@@ -26,5 +32,33 @@ def now_serving(katz_deli)
   end
 end
 
-katz_deli = ["Nate", "Chin", "Shannon", "Karl", "Janell", "Marv"]
+binding.pry
+# katz_deli = ["Nate", "Chin", "Shannon", "Karl", "Janell", "Marv"]
 
+# def line(katz_deli)
+#   new_list = []
+#   katz_deli.each_with_index do |element, index|
+#     new_list << "#{index + 1}. #{element}"
+#   end
+#   if katz_deli.length === 0
+#     puts "The line is currently empty."
+#   else
+#     puts "The line is currently: #{new_list.join(" ")}"
+#   end
+# end
+
+# def take_a_number(katz_deli, end_of_line)
+#   katz_deli << end_of_line
+#   puts "Welcome, #{end_of_line}. You are number #{katz_deli.length} in line."
+# end
+
+# def now_serving(katz_deli)
+#   if katz_deli.length === 0
+#     puts "There is nobody waiting to be served!"
+#   else
+#     puts "Currently serving #{katz_deli[0]}."
+#     katz_deli.shift
+#   end
+# end
+
+# katz_deli = ["Nate", "Chin", "Shannon", "Karl", "Janell", "Marv"]
